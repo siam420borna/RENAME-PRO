06fbc04df1d2f7dd99eca9bbcd6fa87f09ffb6c5
siam141 <115353753+siam141@users.noreply.github.com>
2025-04-15 12:50:40 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7728598881:AAF3evUl3YJw9dp4wm58Ad0oZu1CbeWgMMo")
+API_ID = int(os.environ.get("API_ID", "27752560"))
+API_HASH = os.environ.get("API_HASH", "67d3ec64db8031189962b5d4804884c0")
+OWNER = int(os.environ.get("OWNER", "7862181538"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "@RenamerFile_rebot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "moviehub_botko")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002589776901"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://andfreefirew:andfreefirew@cluster0.qufbkvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
 STRING = os.environ.get("STRING", "")
