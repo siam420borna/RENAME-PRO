d6759bc673c45e1d5835897ee71c3c2bb4f68a94
siam420borna <siam420borna@gmail.com>
2025-04-18 23:45:04 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "@AutoForwarder_rebot")
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "moviehub_botko")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002589776901"))
